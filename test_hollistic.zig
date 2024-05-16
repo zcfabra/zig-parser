35e35409fef9e367d1a1a54f1a97562948c9607f
zcfabra <christianwgnam@gmail.com>
2024-05-15 20:53:21 -0400

    add evaluator to test

--- test_hollistic.zig
+++ test_hollistic.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const tokenizer = @import("tokenizer.zig");
 const parser = @import("parser.zig");
-// const ast = @import("ast.zig");
+const evaluator = @import("evaluator.zig");
 
 const print = std.debug.print;
 
@@ -24,15 +24,15 @@ test "test e2e" {
 test "test nested brackets" {
     var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
     defer arena.deinit();
-    var arena_allocator = arena.allocator();
+    var allocator = arena.allocator();
 
     const src = "((10 + 10) * 100) * ((10 + 10) * 900)";
-    var tk = try tokenizer.Tokenizer.init(src, &arena_allocator);
-    const tokens = try tk.tokenize(&arena_allocator);
-    var prs = parser.Parser.init(tokens, &arena_allocator);
+    var tk = try tokenizer.Tokenizer.init(src, &allocator);
+    const tokens = try tk.tokenize(&allocator);
+    var prs = parser.Parser.init(tokens, &allocator);
     const ast = try prs.parse(parser.Precendence.LOWEST);
-    _ = ast.repr(&arena_allocator) catch {
-        print("Error\n", .{});
-        return;
-    };
+    print("\nAST: {s}\n", .{try ast.repr(&allocator)});
+    var evl = evaluator.Evaluator.init(&allocator);
+    const res = try evl.eval(ast);
+    print("Eval: {s}\n", .{try res.to_string(&allocator)});
 }
